1cd4225d4bd8d21037b53b0926e6b197dfe14424
swaralipi <swara.diya04@gmail.com>
2023-06-04 12:51:04 +0530

    mergeeddd again

--- scripts/faqs.js
+++
@@ -1,9 +0,0 @@
-// $('.faq-list').click(function () {
-  
-//     // $(this).parent('li').toggleClass('the-active').find('.read faq-text').slideToggle();
-//     console.log("hello");
-// });
-
-elems = document.getElementsByClassName("faq-list")[0];
-elem = elems.getElementsByTagName("li");
-Array.from(elem).forEach(element => {element.getElementsByTagName("h4")[0].addEventListener("click", (e)=>{e.target.parentElement.getElementsByTagName('p')[0].classList.toggle("faq-text")})})
